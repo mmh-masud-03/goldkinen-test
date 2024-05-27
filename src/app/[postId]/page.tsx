3d8f19a2fb50fb25340a79090b6e6abce8334375
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-05-27 20:06:30 +0600

    more responsive

--- src/app/[postId]/page.tsx
+++ src/app/[postId]/page.tsx
@@ -19,7 +19,7 @@ const PostDetails = () => {
 
   if (!postDetails) {
     return (
-      <div className="w-1/2 mx-auto mt-4 ">
+      <div className="w-11/12 md:w-1/2 mx-auto mt-4">
         <SkeletonPulse />
         <SkeletonPulse />
       </div>
@@ -27,28 +27,23 @@ const PostDetails = () => {
   }
 
   return (
-    <div className="flex flex-row justify-center">
+    <div className="flex flex-row items-center">
       <Link href={"/"}>
-        <FaArrowLeft className="ml-8 mt-5" />
+        <FaArrowLeft className="ml-4 md:ml-8 mt-5 " />
       </Link>
-      <div className="h-[70vh] w-1/2 mx-auto  border rounded-md shadow-md bg-slate-200 my-5">
-        <div className="flex flex-col bg-slate-100 px-4 py-3 ">
-          <h1 className="font-semibold text-xl py-3">
+      <div className="h-auto md:h-[70vh] w-11/12 md:w-1/2 mx-auto border rounded-md shadow-md bg-slate-200 my-5">
+        <div className="flex flex-col bg-slate-100 px-4 py-3">
+          <h1 className="font-semibold text-lg md:text-xl md:py-3">
             {postDetails.title.toUpperCase()}
           </h1>
-          <div className="flex flex-row justify-between items-center mb-4 ">
+          <div className="flex flex-row justify-between items-center mb-4">
             <p className="flex flex-row gap-1 text-sm items-baseline">
               <FaUser size={40} />
               by
-              <span
-                className="
-          text-base"
-              >
-                {postDetails.author}
-              </span>
+              <span className="text-xs md:text-base">{postDetails.author}</span>
             </p>
 
-            <div className="flex flex-row gap-2 items-center ">
+            <div className="flex flex-col md:flex-row gap-2 items-center mt-2 md:mt-0">
               <p>Share</p>
               <FaFacebook />
               <FaInstagramSquare />
@@ -57,7 +52,7 @@ const PostDetails = () => {
           </div>
         </div>
 
-        <div className="mt-3 px-4">{postDetails.body}</div>
+        <div className="mt-3 px-4 pb-7">{postDetails.body}</div>
       </div>
     </div>
   );
